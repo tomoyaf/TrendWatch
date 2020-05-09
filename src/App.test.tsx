2a814018c373a7e38a0535d02b6e6ce733d4b339
tomoyaf <gazimum@gmail.com>
2020-05-09 17:45:39 +0900

    fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ import App from './App';
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/trend/i);
   expect(linkElement).toBeInTheDocument();
 });
