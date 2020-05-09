63dbf1253989e721ecb64b43a4e5fabba442d830
tomoyaf <gazimum@gmail.com>
2020-05-09 16:31:17 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from './trendwatch.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+      YOU WATCH TREND. TREND WATCHES BACK.
+    </div>
+  );
+}
+
+export default App;
